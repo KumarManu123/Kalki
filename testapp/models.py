a27ab519d05a0710333536e90191b454105e92e9
KumarManu123 <kumarbhukya2012@gmail.com>
2024-06-29 17:57:53 +0530

    first commit

---
+++ testapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    rollno = models.IntegerField()
+    name = models.CharField(max_length=70)
+    dob = models.DateField()
+    marks = models.IntegerField()
+    email = models.EmailField()
+    phonenumber = models.BigIntegerField()
+    address = models.TextField()
